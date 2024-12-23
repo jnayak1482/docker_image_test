daf9bc8058d6101ebb443a4c9d3807fff6d1f801
jinendra_nayak <jnayak2220@gmail.com>
2024-12-23 15:56:53 +0530

    Initial commit with React app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; // Add this import
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          hello jinendra
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
